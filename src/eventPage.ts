698954bfb63edad2f829ad912ce22f59c9295ac6
Sota-Watanabe <stronger.riceball@gmail.com>
2022-11-19 01:29:06 +0900

    content.tsの動作確認

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,11 +1,23 @@
 // Listen to messages sent from other parts of the extension.
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    // onMessage must return "true" if response is async.
-    let isResponseAsync = false;
+  // onMessage must return "true" if response is async.
+  let isResponseAsync = false;
 
-    if (request.popupMounted) {
-        console.log('eventPage notified that Popup.tsx has mounted.');
-    }
+  if (request.popupMounted) {
+    console.log("eventPage notified that Popup.tsx has mounted.");
+  }
 
-    return isResponseAsync;
+  return isResponseAsync;
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
+  if (
+    info.status === "complete" &&
+    tab.url.indexOf("https://app.slack.com") !== -1
+  ) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id, allFrames: true },
+      files: ["./js/content.js"],
+    });
+  }
 });
