722aa699bafde3b8a4ab50818ac6a820c9089a61
Sota-Watanabe <stronger.riceball@gmail.com>
2022-11-20 17:44:51 +0900

    初期表示時にもスクリプト起動

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -10,14 +10,29 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   return isResponseAsync;
 });
 
+
+// 後にリファクタ
 chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
   if (
     info.status === "complete" &&
     tab.url.indexOf("https://app.slack.com") !== -1
   ) {
+    console.log("tabsOnupdated");
     chrome.scripting.executeScript({
       target: { tabId: tab.id, allFrames: true },
       files: ["./js/content.js"],
     });
   }
 });
+
+chrome.webRequest.onCompleted.addListener(
+  function (details) {
+    if (details.url.indexOf("https://app.slack.com") !== -1 && details.tabId > 0)  {
+      chrome.scripting.executeScript({
+        target: { tabId: details.tabId, allFrames: true },
+        files: ["./js/content.js"],
+      });
+    }
+  },
+  { urls: ["https://app.slack.com/*"] }
+);
