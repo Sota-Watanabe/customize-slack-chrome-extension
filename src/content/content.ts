71abaa94431ec1153a02c41521870d2891814f99
Sota-Watanabe <stronger.riceball@gmail.com>
2022-11-20 16:45:45 +0900

    テスト

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,1 +1,24 @@
-console.log('aaa')
+import { Exception } from "node-sass";
+
+window.addEventListener("complete", function () {
+  const re = /\/client\/.+?\/unreads.?/;
+  if (re.test(location.pathname)) {
+    console.log("start");
+    const a = document.querySelector(`.p-unreads_view`);
+    //   const a = document.querySelectorAll(`.c-virtual_list__sticky_container`);
+    console.log(a);
+    console.log("afsda");
+  }
+});
+
+
+// window.addEventListener("load", function () {
+//   const re = /\/client\/.+?\/unreads.?/;
+//   if (re.test(location.pathname)) {
+//     console.log("start");
+//     const a = document.querySelector(`.p-unreads_view`);
+//     //   const a = document.querySelectorAll(`.c-virtual_list__sticky_container`);
+//     console.log(a);
+//     console.log("afsda");
+//   }
+// });
