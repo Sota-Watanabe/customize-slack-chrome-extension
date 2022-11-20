79f155971edc1c2d558dd721a9472c0d6cd3ce54
Sota-Watanabe <stronger.riceball@gmail.com>
2022-11-20 19:21:17 +0900

    unreadContainers の動作確認

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,24 +1,13 @@
 import { Exception } from "node-sass";
 
-window.addEventListener("complete", function () {
-  const re = /\/client\/.+?\/unreads.?/;
-  if (re.test(location.pathname)) {
-    console.log("start");
-    const a = document.querySelector(`.p-unreads_view`);
-    //   const a = document.querySelectorAll(`.c-virtual_list__sticky_container`);
-    console.log(a);
-    console.log("afsda");
+const re = /\/client\/.+?\/unreads.?/;
+if (re.test(location.pathname)) {
+  const unreadContainers = document.querySelectorAll(
+    `.c-virtual_list__sticky_container:has([id^=unreads_view_header-])`
+  ) as unknown as HTMLCollectionOf<HTMLElement>;
+
+  for (const container of Array.from(unreadContainers)) {
+    console.log(container)
+    container.style.color = 'red';
   }
-});
-
-
-// window.addEventListener("load", function () {
-//   const re = /\/client\/.+?\/unreads.?/;
-//   if (re.test(location.pathname)) {
-//     console.log("start");
-//     const a = document.querySelector(`.p-unreads_view`);
-//     //   const a = document.querySelectorAll(`.c-virtual_list__sticky_container`);
-//     console.log(a);
-//     console.log("afsda");
-//   }
-// });
+}
