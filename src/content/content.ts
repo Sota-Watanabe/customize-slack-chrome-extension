a230d654d226699c91588b91f60199af6e8cd0ef
Sota-Watanabe <stronger.riceball@gmail.com>
2022-11-20 23:29:45 +0900

    スクロールできるように padding 追加

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,10 +1,15 @@
-import { Exception } from "node-sass";
-
 const re = /\/client\/.+?\/unreads.?/;
 if (re.test(location.pathname)) {
   const unreadContainers = document.querySelectorAll(
     `.c-virtual_list__sticky_container:has([id^=unreads_view_header-])`
   ) as unknown as HTMLCollectionOf<HTMLElement>;
+
+  const setReadContainer = unreadContainers[0].parentElement
+    .lastElementChild as HTMLElement;
+  console.log(window.innerHeight as unknown as string);
+  setReadContainer.style.paddingBottom = `${
+    window.innerHeight as unknown as string
+  }px`;
 
   for (const container of Array.from(unreadContainers)) {
     console.log(container)
