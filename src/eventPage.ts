8def2b3b23341c7c5692cef006e4dbbf79903f2f
Sota-Watanabe <stronger.riceball@gmail.com>
2022-11-22 01:45:11 +0900

    webRequest.onCompleted を利用

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -12,27 +12,27 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 
 // 後にリファクタ
-chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
-  if (
-    info.status === "complete" &&
-    tab.url.indexOf("https://app.slack.com") !== -1
-  ) {
-    console.log("tabsOnupdated");
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id, allFrames: true },
-      files: ["./js/content.js"],
-    });
-  }
-});
+// chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
+//   if (
+//     info.status === "complete" &&
+//     tab.url.indexOf("https://app.slack.com") !== -1
+//   ) {
+//     console.log("tabsOnupdated");
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id, allFrames: true },
+//       files: ["./js/content.js"],
+//     });
+//   }
+// });
 
-// chrome.webRequest.onCompleted.addListener(
-//   function (details) {
-//     if (details.url.indexOf("https://app.slack.com") !== -1 && details.tabId > 0)  {
-//       chrome.scripting.executeScript({
-//         target: { tabId: details.tabId, allFrames: true },
-//         files: ["./js/content.js"],
-//       });
-//     }
-//   },
-//   { urls: ["https://app.slack.com/*"] }
-// );
+chrome.webRequest.onCompleted.addListener(
+  function (details) {
+    if (details.url.indexOf("https://app.slack.com") !== -1 && details.tabId > 0)  {
+      chrome.scripting.executeScript({
+        target: { tabId: details.tabId, allFrames: true },
+        files: ["./js/content.js"],
+      });
+    }
+  },
+  { urls: ["https://app.slack.com/*"] }
+);
