91facfc410d94f318c727897ff7638ee04ad7d35
Sota-Watanabe <stronger.riceball@gmail.com>
2022-11-22 15:22:14 +0900

    リファクタ、細かい機能追加

--- src/content/content.ts
+++ src/content/content.ts
@@ -2,27 +2,26 @@
 function callback(mutationsList, observer) {
   console.log("----------");
   for (const mutation of mutationsList) {
+    const addedNode = mutation.addedNodes[0];
+    if (!addedNode) continue;
+    // console.log(addedNode.id);
     // 処理
-    if (
-      mutation.addedNodes[0] &&
-      mutation.addedNodes[0].id.indexOf("unreads_view_spacer-bottom-") === 0
-    ) {
-      const unreadBottom = mutation.addedNodes[0];
-      const unreadId = unreadBottom.id.split("-").slice(-1)[0];
+    if (addedNode.id.indexOf("unreads_view_spacer-bottom-") === 0) {
+      const unreadId = addedNode.id.split("-").slice(-1)[0];
       // onceは一回だけ実行する時用です
-      let once = false;
+      let observing = true;
 
       // スクロール時に非表示になったら既読ボタンクリック
-      window.addEventListener(
+      document.addEventListener(
         "scroll",
         function () {
-          const containerTop = unreadBottom.getBoundingClientRect().top;
+          const containerTop = addedNode.getBoundingClientRect().top;
           if (containerTop === 0) return;
 
           // TODO: 160 を可変にできるように、
           // ヘッダー + 未読 + 未読ヘッダー + container高さ(unreadBottom.clientHeight) = 160くらい
-          if (containerTop < 160 - unreadBottom.clientHeight && !once) {
-            once = true;
+          if (containerTop < 160 - addedNode.clientHeight && observing) {
+            observing = false;
             const readBtn = document.querySelector(
               `#unreads_view_header-${unreadId} > div > span > button`
             ) as HTMLElement;
@@ -31,11 +30,34 @@ function callback(mutationsList, observer) {
         },
         { capture: true }
       );
+      continue;
     }
+    // 「○件の新しいメッセージ」ボタンを自動クリック
+    if (addedNode.id.indexOf("unreads_view_show-newer-") === 0) {
+      addedNode.firstElementChild.click();
+      continue;
+    }
+    // 最後までスクロールできるようパディング追加
+    if (addedNode.id === "unreads_view_footer") {
+      addedNode.firstElementChild.style.paddingBottom = `${
+        window.innerHeight as unknown as string
+      }px`;
+
+      continue;
+    }
+    // break;
   }
   //ターゲット要素の監視を停止
   // obs.disconnect();
 }
+
+// TODO: 何処かでこれを実行
+const autoNewMessageShow = () => {
+  const showBtn = document.querySelector(
+    `.p-unreads_view__empty--show_new > button`
+  ) as HTMLElement;
+  if (showBtn) showBtn.click();
+};
 
 // 未読ページか確認
 const re = /\/client\/.+?\/unreads.?/;
