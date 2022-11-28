85342b35d02046b04f70adc3991923ad9ea444fb
Sota-Watanabe <stronger.riceball@gmail.com>
2022-11-28 19:16:29 +0900

    undefined のエラー対処

--- src/content/content.ts
+++ src/content/content.ts
@@ -21,14 +21,14 @@ function setEmptyObs() {
     const emptyObs = new MutationObserver((mutationsList) => {
       // 新しいメッセージボタンの自動クリック
       if (
-        (mutationsList[0].previousSibling as unknown as HTMLElement).className ===
-          "p-unreads_view__empty__message" &&
-        (mutationsList[0].addedNodes[0] as unknown as HTMLElement).className ===
+        (mutationsList[0].previousSibling as unknown as HTMLElement)
+          ?.className === "p-unreads_view__empty__message" &&
+        (mutationsList[0]?.addedNodes[0] as unknown as HTMLElement)?.className ===
           "c-button c-button--primary c-button--medium"
       ) {
-        (mutationsList[0].addedNodes[0] as HTMLElement).click();
+        (mutationsList[0]?.addedNodes[0] as HTMLElement).click();
       } else if (
-        (mutationsList[1].addedNodes[0] as HTMLElement).className ===
+        (mutationsList[1]?.addedNodes[0] as HTMLElement)?.className ===
         "p-unreads_view"
       ) {
         setUnreadParentObs();
@@ -52,7 +52,6 @@ function setUnreadParentObs() {
   if (unreadParent) {
     // 未読コンテナにイベントをバインドする
     for (const child of unreadParent.childNodes as unknown as HTMLElement[]) {
-      console.log(child);
       bindAutoReadEvent(child);
     }
 
