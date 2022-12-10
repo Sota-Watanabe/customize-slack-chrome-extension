1f6b2f53c7df44604af81859ff8a8ac1fb639dff
Sota-Watanabe <stronger.riceball@gmail.com>
2022-12-11 04:16:33 +0900

    keyを利用

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,5 +1,6 @@
+import { ConfigOptionsKey } from "../domains/models/popup";
 import scrollAutoRead from "./scroll-auto-read";
 
-chrome.storage.local.get("scrollAutoRead", function (config) {
+chrome.storage.local.get(ConfigOptionsKey, function (config) {
   if (config.scrollAutoRead) scrollAutoRead();
 });
